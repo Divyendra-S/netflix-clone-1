35a67f6c25aa498beaf12d44a52f8153dab364c4
divyendra <betudst9107@gmail.com>
2023-11-19 16:40:00 +0530

    sssssss

--- src/components/manageAccounts/index.js
+++ src/components/manageAccounts/index.js
@@ -43,7 +43,7 @@ export const ManageAccounts = () => {
     setLoader(true);
     try {
       const result = await Axios.get(
-        `api/getAllAccounts?id=${session?.user?.uid}`
+        `/api/getAllAccounts?id=${session?.user?.uid}`
       );
       if (result.data && result.data.message) {
         setAccount(result.data.message);
