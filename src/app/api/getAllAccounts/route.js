befffa40beaf311fa57f098bf2fd60b3625810a9
divyendra <betudst9107@gmail.com>
2023-11-19 17:01:49 +0530

    ss

--- src/app/api/getAllAccounts/route.js
+++ src/app/api/getAllAccounts/route.js
@@ -9,7 +9,7 @@ export async function GET(req) {
     const searchParams = req.nextUrl.searchParams;
     const id = searchParams.get("id");
     console.log("iddddddd", id);
-    const users = await User.find({uid: id});
+    const users = await User.find();
     return NextResponse.json({
       sucess: true,
       message: users,
@@ -22,4 +22,4 @@ export async function GET(req) {
     });
   }
 }
-//
+//{uid: id}
