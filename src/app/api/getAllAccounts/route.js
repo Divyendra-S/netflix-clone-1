547888176a193d978e81236c7d4e1908066e6061
divyendra <betudst9107@gmail.com>
2023-12-02 18:19:39 +0530

    vv

--- src/app/api/getAllAccounts/route.js
+++ src/app/api/getAllAccounts/route.js
@@ -2,12 +2,16 @@ import connectDb from "@/database";
 import User from "@/models/Account";
 import { NextResponse } from "next/server";
 
+
+const getId = (reqs)=>{
+  const searchParams = reqs.nextUrl.searchParams;
+    const id = searchParams.get("id");
+}
 export async function GET(req) {
   try {
     await connectDb();
     //const { searchParams } = new URL(req.url);
-    const searchParams = req.nextUrl.searchParams;
-    const id = searchParams.get("id");
+    getId(req);
     console.log("iddddddd", id);
     const users = await User.find({ uid: id });
     return NextResponse.json({
