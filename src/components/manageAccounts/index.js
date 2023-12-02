09cd81879667fa7df2f4d9873efca04f5fe8fc3d
divyendra <betudst9107@gmail.com>
2023-12-03 01:16:47 +0530

    ss

--- src/components/manageAccounts/index.js
+++ src/components/manageAccounts/index.js
@@ -24,6 +24,9 @@ const initialForm = {
   pin: "",
 };
 
+// Opt out of caching for all data requests in the route segment
+export const dynamic = 'force-dynamic'
+
 export const ManageAccounts = () => {
   const { Account, setAccount, loader, setLoader, setLoggedIn } =
     useContext(AppContext);
