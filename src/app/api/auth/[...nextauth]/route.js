c35b96812b20eaf6a7d57ceb02f0c7a2079ba164
divyendra <betudst9107@gmail.com>
2023-12-06 21:01:00 +0530

    Changed loggIn Provider

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 const authOptions = {
   providers: [
